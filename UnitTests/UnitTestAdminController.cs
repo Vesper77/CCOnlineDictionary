5195cde926571ba6b1abe9b5bafe42f95eb9ba83
LLosstarot <Valerius.96@mail.ru>
2016-07-05 23:42:10 +0400

    UnitTests

---
+++ UnitTests/UnitTestAdminController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Web.Mvc;
+using OnlineDiary.Controllers;
+using NUnit.Framework;
+using OnlineDiary.Models.CRUDViewModels;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class UnitTestAdminController
+    {
+        [TestMethod]
+        public void Index_Return_ActionResults()
+        {
+            AdminController controller = new AdminController();
+            var result = controller.Index(2) as ViewResult;
+            var s = (IndexUserViewModel) result.ViewData.Model;
+            Assert.AreEqual("all", s.Role);
+        }
+    }
+}
